406db068f211ccd8aaf31b27829debe93607d1e9
Sudarshen <sudarshenk@gmail.com>
2025-12-22 21:26:47 +0100

    Well Hellow theyre beauty, do you come here often? today's lesson is about OOP, what it is and why it exist with a couple example's.

--- ZTM -  P_Course - OOP.py
+++ ZTM -  P_Course - OOP.py
@@ -1,1 +1,34 @@
 print("Hellow world!")
+
+
+# ////////////////////////////////////////////////////////////////
+# Lesson: What Is OOP? 
+# Object oriented programming is creating your own data type objects and working with it. custom data types. 
+
+# Object-oriented programming means you create your own data types and write your program by working with those data types. Those data types also define what can be done with the data. 
+
+# Why is this usefull? example, Amazone -> delivery drones needed to transport packages. because complicated. to make code more manageable, use OOP. easier to maintain, extend and write. example, to code a custom data type: object; which is the propellors which allows the drone to fly, another example is signalling to signal and give status. another dev works on camera and works on the vision of the drone. this breaks up the funtionality in different pieces, that model the real world into seperate objects. so that different people can work on different parts and then they can combine them afterwards. 
+
+# OOP is a method of dividing and qonquiring in programming where you can seperate a whole goal/functionality into smaller objects and work to complete the objective in organized manner trough the use of creating different custom data types and then combining them. 
+# ////////////////////////////////////////////////////////////////
+
+
+
+# ////////////////////////////////////////////////////////////////
+# Lesson:
+
+
+
+
+
+
+
+
+
+
+# ////////////////////////////////////////////////////////////////
+
+
+# ////////////////////////////////////////////////////////////////
+# Lesson:
+# ////////////////////////////////////////////////////////////////
